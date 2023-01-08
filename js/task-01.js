e2fcd6b130394104d0d37cdbe325ff78e40d4ae6
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-08 20:59:34 +0200

    01-02

--- js/task-01.js
+++ js/task-01.js
@@ -3,12 +3,15 @@ const refs = {
     items: document.querySelectorAll('.item'),
 }
 
-console.log("Number of categories", refs.categories.childElementCount);
+console.log("Number of categories:", refs.categories.childElementCount);
 
 refs.items.forEach((el) => {
+
+    // Вариант1
     // console.log("Category:", el.querySelector("h2").textContent);
     // console.log("Elements:", el.querySelector("ul").childElementCount);
 
+    // Вариант2
     console.log(`
 Category: ${el.querySelector("h2").textContent}
 Elements: ${el.querySelector("ul").childElementCount}`);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refs = {
+  ingredients: document.querySelector('#ingredients'),
+}
+
+const arr = []
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredients[i];
+  arr.push(li)
+}
+
+// console.log(arr);
+refs.ingredients.append(...arr)
