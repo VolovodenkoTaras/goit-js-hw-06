3f38d3d4420513d62336428974ebabd518f31979
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-12 00:58:26 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,35 +5,36 @@ function getRandomHexColor() {
 const refs = {
   buttonCreate: document.querySelector("[data-create]"),
   buttonDestroy: document.querySelector("[data-destroy]"),
-  input: document.querySelector('input[type="number"]'),
-  div: document.querySelector("#boxes"),
+  inputValue: document.querySelector('input[type="number"]'),
+  divBoxes: document.querySelector("#boxes"),
 }
 
 let size = 30;
-let amount = 0;
+let inputNum = 0;
 
 const onInput = (event) => {
-  amount = event.currentTarget.value;
+  inputNum = event.currentTarget.value;
 };
 
-const createBoxes = () => {
+function createBoxes(amount) {
+  amount = inputNum;
   const arrayDivs = [];
   for (let i = 0; i < amount; i += 1) {
     const backgroundColor = getRandomHexColor();
     arrayDivs.push(`<div class="box" style="width: ${size}px; height: ${size}px; background-color: ${backgroundColor}"></div>`);
     size += 10;
   }
-  refs.div.insertAdjacentHTML("beforeend", arrayDivs.join(""));
+  refs.divBoxes.insertAdjacentHTML("beforeend", arrayDivs.join(""));
 };
 
 const destroyBoxes = () => {
-  refs.input.value = "";
-  refs.div.innerHTML = "";
+  refs.inputValue.value = "";
+  refs.divBoxes.innerHTML = "";
   size = 30;
-  amount = 0;
+  inputNum = 0;
 };
 
-refs.input.addEventListener("input", onInput);
+refs.inputValue.addEventListener("change", onInput);
 refs.buttonCreate.addEventListener("click", createBoxes);
 refs.buttonDestroy.addEventListener("click", destroyBoxes);
 
