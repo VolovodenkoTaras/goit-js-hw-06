ed0765b2355711522833efebcc4e0743276a3a0d
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-11 22:32:18 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (!email.value || !password.value) {
+        return alert("Please fill in all the fields!");
+    } else {
+        const inputData = { email: email.value, password: password.value }
+        console.log(inputData);
+        event.currentTarget.reset();
+    }
+}
