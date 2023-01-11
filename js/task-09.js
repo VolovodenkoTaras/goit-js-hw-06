89d2a4b3d7a366ee9e8cad14a3707461583d8172
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-11 22:52:31 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const buttonRef = document.querySelector(".change-color");
+const spanTextRef = document.querySelector(".color");
+
+const onButtonClick = () => {
+  const bodyColor = getRandomHexColor();
+  spanTextRef.textContent = bodyColor;
+  document.body.style.backgroundColor = bodyColor;
+};
+
+buttonRef.addEventListener("click", onButtonClick);
